a530650f62fd4ab411c1d51fbc66dfbb7633512d
Genevieve Nuebel <72330302+Gvieve@users.noreply.github.com>
2025-11-21 15:36:29 -0700

    Multi-Version SDK Generation POC (#66)
    * Create version specific static config fies
    
    * Create new workflows to test multi version POC support
    
    * Update the config/workflows since must merge to master to test
    
    * Update config files to latest version
    
    ---------
    
    Co-authored-by: Genevieve Nuebel <genevieve.nuebel@mx.com>

--- .github/clean.rb
+++ .github/clean.rb
@@ -11,6 +11,9 @@ ALLOW_LIST = [
 
 ::Dir.each_child(::Dir.pwd) do |source|
   next if ALLOW_LIST.include?(source)
+  
+  # Preserve test-output directories for multi-version POC testing
+  next if source.start_with?("test-output-")
 
   ::FileUtils.rm_rf("#{::Dir.pwd}/#{source}")
 end
